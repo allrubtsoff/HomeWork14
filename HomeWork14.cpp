91cb2890d241f76980855dbc391cb0da8d433c46
allrubtsoff <allrubtsooff@yandex.ru>
2021-04-21 21:50:44 +0300

    Добавьте файлы проекта.

---
+++ HomeWork14.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    name = "alexander";
+    char ch = name[0];
+    char ch2 = name[8];
+
+    std::cout << name << "\n" 
+        << name.length() << "\n" 
+        << name[0] << "\n" 
+        << name[8] <<"\n";
+    
+}
+
